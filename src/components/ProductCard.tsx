366e1b7d76b0ab0c28ac77d2226377c0985acb23
Raju Sarvasiddi <rajusvsp@gmail.com>
2025-10-01 19:39:08 +0530

    product card turned as reusable component

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import TruncateText from "../utils/TruncateText";
+import "./ProductCard.css";
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  thumbnail: string;
+  rating: number;
+};
+
+const ProductCard = ({ product }: { product: Product }) => {
+  return (
+    <div className="product-card">
+      <Link to={`/products/${product.id}`}>
+        <img src={product.thumbnail} alt={product.name} />
+      </Link>
+      <h3>{product.name}</h3>
+      <p>{TruncateText(product.description, 70)}</p>
+      <p className="price">${product.price.toFixed(2)}</p>
+      <div className="rating">
+        Rating {product.rating}
+        {/* You can plug in renderStars(product.rating) here */}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./Products.css";
-import TruncateText from "../../utils/TruncateText";
-import { Link } from "react-router-dom";
+import ProductCard from "../../components/ProductCard";
 
 type Product = {
   id: number;
@@ -29,15 +27,16 @@ const Products = () => {
         <h1>Our Products</h1>
         <div className="product-grid">
           {products.map((product) => (
-            <div className="product-card" key={product.id}>
-              <Link to={`/products/${product.id}`}>
-                <img src={product.thumbnail} alt={product.name} />
-              </Link>
-              <h3>{product.name}</h3>
-              <p>{TruncateText(product.description, 70)}</p>
-              <p>{product.rating}</p>
-              <span className="price">${product.price.toFixed(2)}</span>
-            </div>
+            // <div className="product-card" key={product.id}>
+            //   <Link to={`/products/${product.id}`}>
+            //     <img src={product.thumbnail} alt={product.name} />
+            //   </Link>
+            //   <h3>{product.name}</h3>
+            //   <p>{TruncateText(product.description, 70)}</p>
+            //   <p>{product.rating}</p>
+            //   <span className="price">${product.price.toFixed(2)}</span>
+            // </div>
+            <ProductCard key={product.id} product={product} />
           ))}
         </div>
       </div>
