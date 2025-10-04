7b4c640bf44a62f9bb0a17490cbb2ce9afcdc924
Raju Sarvasiddi <rajusvsp@gmail.com>
2025-10-04 19:48:44 +0530

    [master] Add route - keep outlet to render

--- src/components/PublicLayout.tsx
+++ src/components/PublicLayout.tsx
@@ -1,16 +1,60 @@
-// src/components/PublicLayout.tsx
-import { Outlet } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Link as MuiLink,
+} from "@mui/material";
+import React from "react";
+import {
+  NavLink,
+  Outlet,
+  LinkProps as RouterLinkProps,
+} from "react-router-dom";
+const NavLinkBehavior = React.forwardRef<HTMLAnchorElement, RouterLinkProps>(
+  (props, ref) => <NavLink ref={ref} {...props} />
+);
 
 const PublicLayout = () => {
+  const navLinks = [
+    { label: "About", path: "about" },
+    { label: "Login", path: "login" },
+  ];
+
   return (
-    <Box sx={{ padding: 4 }}>
-      <Typography variant="h4" gutterBottom>
-        Welcome to GMS
-      </Typography>
-      {/* You can add a public header, footer, or branding here */}
+    <>
+      <AppBar position="sticky">
+        <Toolbar
+          sx={{ display: "flex", justifyContent: "space-between" }}
+          className="toolbarBorder"
+        >
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <img
+              src="/logo192.png"
+              alt="GMS Logo"
+              style={{ width: 40, height: 40, objectFit: "contain" }}
+            />
+            <Typography variant="h6" sx={{ fontWeight: 600 }}>
+              GMS
+            </Typography>
+          </Box>
+
+          <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+            {/* Regular links using MuiLink */}
+            {navLinks.map((item) => (
+              <MuiLink
+                key={item.path}
+                component={NavLinkBehavior}
+                to={item.path}
+              >
+                {item.label}
+              </MuiLink>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
       <Outlet />
-    </Box>
+    </>
   );
 };
 
