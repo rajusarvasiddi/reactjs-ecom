b6fa073a9737b378f316c720adbd83d1a2a04a8c
Raju Sarvasiddi <rajusvsp@gmail.com>
2025-10-21 12:40:25 +0530

    [master] Added work log form

--- src/pages/JobCard/steps/WorkProgressStep.tsx
+++ src/pages/JobCard/steps/WorkProgressStep.tsx
@@ -1,4 +1,11 @@
-import { Typography } from "@mui/material";
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const WorkProgressStep = () => {
   return (
@@ -11,14 +18,61 @@ const WorkProgressStep = () => {
       >
         Work Progress
       </Typography>
-      <div>Work Started On (DateTime)</div>
-      <div>Work Completed On (DateTime)</div>
-      <div>
-        Work Status (Dropdown: Pending / In Progress / Completed / On Hold)
-      </div>
-      <div>Parts Used (Multiselect)</div>
-      <div>Labor Hours Spent</div>
-      <div>Remarks / Issues Encountered</div>
+      <Grid container spacing={2}>
+        <Grid size={4} sx={{ mb: 2 }}>
+          <TextField
+            fullWidth
+            label="Work Started On (DateTime)"
+            size="small"
+            autoComplete="off"
+          />
+        </Grid>
+        <Grid size={4} sx={{ mb: 2 }}>
+          <TextField
+            fullWidth
+            label="Work Completed On (DateTime)"
+            size="small"
+            autoComplete="off"
+          />
+        </Grid>
+        <Grid size={4} sx={{ mb: 2 }}>
+          <FormControl fullWidth size="small">
+            <InputLabel id="work-status-label">Work Status</InputLabel>
+            <Select labelId="work-status-label" label="Work Status">
+              <option value="">-Select-</option>
+              <option value="in-progress">In Progress</option>
+              <option value="completed">Completed</option>
+              <option value="on-hold">On Hold</option>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid size={4} sx={{ mb: 2 }}>
+          <TextField
+            fullWidth
+            label="Parts Used"
+            size="small"
+            autoComplete="off"
+          />
+        </Grid>
+        <Grid size={4} sx={{ mb: 2 }}>
+          <TextField
+            fullWidth
+            label="Labor Hours Spent"
+            size="small"
+            autoComplete="off"
+          />
+        </Grid>
+        <Grid size={12} sx={{ mb: 2 }}>
+          <TextField
+            fullWidth
+            multiline
+            rows={4}
+            label="Remarks / Issues Encountered"
+            size="small"
+            autoComplete="off"
+          />
+        </Grid>
+      </Grid>
     </>
   );
 };
